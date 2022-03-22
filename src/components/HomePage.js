b72fbfe7e8bebb6c735bc4029af501c0ab54ad65
djela98 <vucendicandjela98@gmail.com>
2022-03-22 01:26:26 +0100

    Kreirana aplikacija, rutiranje u App.js, komponente HomePage i Navbar, importovan Bootstrap,stil u App.css

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar'
+
+function HomePage() {
+
+    const natpis = "ITEH - React - Treći domaći zadatak"
+
+    return (
+        <div className="homepage">
+            <Navbar />
+
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+
+    return (
+        <div className="navbar">
+            <a className="active" href="/">Home</a>
+        </div>
+    );
+}
+
+export default Navbar;
